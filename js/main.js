4a0df54847c370a1fb7930a25fce58e9c0791ef9
tuanh <tuanh.beu@gmail.com>
2015-04-25 15:06:12 +0700

    first github commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by tuanh_000 on 4/25/2015.
+ */
+
+$(document).ready(function(e) {
+    $("#tag-header").load("tags/header.html");
+    $("#tag-footer").load("tags/footer.html");
+    $("#tag-section-contact").load("tags/section-contact.html");
+    $("#tag-section-recent-works").load("tags/section-recent-works.html");
+})
